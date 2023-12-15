57ceacb5e6bc5e36b30a3857b464f1550b142fba
Darrenwu1006 <wuy529@newschool.edu>
2023-12-15 09:58:56 -0500

    file adjustment

--- script/map.js
+++ script/map.js
@@ -234,3 +234,113 @@ resetZoomButton.addEventListener('click', function () {
     // pitch: 0,
   });
 });
+
+//let books interact with the map markers
+
+document.addEventListener('DOMContentLoaded', function () {
+  let geojson;
+  let currentMarkers = [];
+
+  // Fetch GeoJSON data
+  fetch('./Data/merged_df.geojson')
+    .then((response) => response.json())
+    .then((data) => {
+      geojson = data;
+
+      geojson.features.forEach(function (feature) {
+        try {
+          var originalTitle = feature.properties.Title;
+          var cleanedTitle = originalTitle.replace(/[\s\W]/g, '').toLowerCase();
+          feature.properties.Title = cleanedTitle;
+        } catch (error) {
+          console.error(
+            'Error processing feature:',
+            feature,
+            'Error:',
+            error.message
+          );
+        }
+      });
+    });
+
+  // Handle click on image
+  function handleClick(img) {
+    let bookTitle = img
+      .getAttribute('alt')
+      .toLowerCase()
+      .replace(/[\s\W]/g, '');
+    console.log(bookTitle);
+
+    // Find the features in GeoJSON
+    let features = geojson.features.filter(function (feature) {
+      try {
+        var cleanedTitle = feature.properties.Title;
+        return cleanedTitle === bookTitle;
+      } catch (error) {
+        console.error(
+          'Error filtering feature:',
+          feature,
+          'Error:',
+          error.message
+        );
+        return false; // Skip this feature
+      }
+    });
+
+    // Check if features are found
+    if (features.length > 0) {
+      // Clear current markers
+      currentMarkers.forEach(function (marker) {
+        marker.remove();
+      });
+      currentMarkers = [];
+
+      // Add markers to the map for each feature
+      features.forEach(function (feature) {
+        try {
+          // Extract coordinates from GeoJSON geometry
+          let coordinates = feature.geometry.coordinates[0][0];
+
+          let marker = new mapboxgl.Marker({
+            element: createCustomMarkerElement(),
+          })
+
+            .setLngLat(coordinates)
+            .addTo(map);
+
+          function createCustomMarkerElement() {
+            let customMarkerElement = document.createElement('div');
+            customMarkerElement.className = 'map-marker'; 
+            customMarkerElement.style.backgroundImage =
+              'url(./png/icon5-removebg-preview.png)';
+            customMarkerElement.style.width = '50px'; 
+            customMarkerElement.style.height = '50px'; 
+            return customMarkerElement;
+          }
+
+          // Add marker to the currentMarkers array
+          currentMarkers.push(marker);
+
+          console.log('Marker added for:', bookTitle);
+        } catch (error) {
+          console.error(
+            'Error adding marker for feature:',
+            feature,
+            'Error:',
+            error.message
+          );
+        }
+      });
+    } else {
+      console.log('No features found for:', bookTitle);
+    }
+  }
+
+  var imgElements = document.querySelectorAll('.bookTitle');
+
+  imgElements.forEach(function (img) {
+    img.addEventListener('click', function () {
+      handleClick(img);
+    });
+  });
+});
